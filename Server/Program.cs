4a577487627e4ce882d5e3fd4e3d16d576385734
Sindre Kroknes <shkroknes@maritech.no>
2022-02-01 23:15:25 +0100

    disable response compression in dev

--- Server/Program.cs
+++ Server/Program.cs
@@ -160,12 +160,12 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
+    app.UseResponseCompression();
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseResponseCompression();
 app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
