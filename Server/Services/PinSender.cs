f3ebd772590dbf185a2fd0cb60b6e64e9b70fe32
Sindre Kroknes <shkroknes@maritech.no>
2024-08-13 21:13:17 +0200

    removed references to "Romsdal og Nordmøre krets"

--- Server/Services/PinSender.cs
+++ Server/Services/PinSender.cs
@@ -35,7 +35,7 @@ public class PinSender : IPinSender
         if (string.IsNullOrEmpty(delegat.Epost))
             throw new StemmeException($"Delegat {delegat.Delegatnummer} ({delegat.Navn}) har ikke e-postadresse");
             
-        var msg = $"Hei. {delegat.Navn} har PIN-kode {delegat.Delegatkode} i Romsdal og Nordmøre krets sitt system for elektronisk avstemning. Bruk PIN-koden på {baseUrl} for å avgi din stemme i de sakene der du har stemmerett. Du kan også bruke lenken: {baseUrl}/pin/{delegat.Delegatkode}";
+        var msg = $"Hei {delegat.Navn}. Din PIN-kode for avstemming på {delegat.Arrangement.Navn} er: {delegat.Delegatkode}. Bruk PIN-koden på {baseUrl} for å avgi din stemme i de sakene der du har stemmerett. Du kan også bruke lenken: {baseUrl}/pin/{delegat.Delegatkode}";
         var model = new EpostModel(delegat.Epost, $"PIN-kode til {delegat.Arrangement.Navn}")
         {
             TilNavn = delegat.Navn,
@@ -51,8 +51,7 @@ public class PinSender : IPinSender
             _logger.LogError(e, "Feil ved sending av e-post til {Delegat}", delegat.Navn);
             return new SendPinResult(false);
         }
-        
-        
+
         delegat.SendtEmailInternal = DateTimeOffset.UtcNow;
         await _context.SaveChangesAsync(cancellationToken);
         return new SendPinResult(true);
@@ -63,6 +62,7 @@ public class PinSender : IPinSender
         var (delegatId, baseUrl) = request;
         var delegat = await _context.Delegat
             .Where(d => d.Id == delegatId)
+            .Include(d => d.Arrangement)
             .FirstOrDefaultAsync(cancellationToken: cancellationToken);
         if (delegat == null)
             throw new StemmeException($"Fant ikke delegat med id{delegatId}");
@@ -70,7 +70,7 @@ public class PinSender : IPinSender
             throw new StemmeException($"Delegat {delegat.Delegatnummer} ({delegat.Navn}) har ikke telefonnummer");
 
         var uri = new Uri(baseUrl, UriKind.Absolute);
-        var msg = $"Hei. {delegat.Navn} har PIN-kode {delegat.Delegatkode} i Romsdal og Nordmøre krets sitt system for elektronisk avstemning. Bruk PIN-koden på {uri} for å avgi din stemme. Du kan også bruke lenken: {new Uri(uri,$"pin/{delegat.Delegatkode}")}";
+        var msg = $"Hei {delegat.Navn}. Din PIN-kode for avstemming på {delegat.Arrangement.Navn} er {delegat.Delegatkode}. Bruk PIN-koden på {uri} for å avgi din stemme. Du kan også bruke lenken: {new Uri(uri,$"pin/{delegat.Delegatkode}")}";
 
         var success = await _smsSender.SendSms(delegat.Telefon, msg);
         if (success)
